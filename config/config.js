b7b179312acd17474e8b6ac14536943838868abe
afituri <ahmed.elfituri@gmail.com>
2015-12-12 19:28:18 +0200

    first commit

---
+++ config/config.js
@@ -0,0 +1,25 @@
+{
+  "development": {
+    "dialect": "mysql",
+    "username": "root",
+    "password": "102030",
+    "database": "education",
+    "host": "localhost",
+    "logging": false
+
+  },
+  "test": {
+    "username": "root",
+    "password": "102030",
+    "database": "booking_test",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "102030",
+    "database": "booking_production",
+    "host": "localhost",
+    "dialect": "mysql"
+  }
+}
