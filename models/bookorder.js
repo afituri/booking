8698da2ca7b347ca894a9786811309497a703b90
ofituri <omar.fituri@gmail.com>
2015-12-13 19:36:00 +0200

    bookorder

--- models/bookorder.js
+++ models/bookorder.js
@@ -2,11 +2,15 @@
 
 module.exports = function(sequelize, DataTypes) {
   var BookOrder = sequelize.define("BookOrder", {
-    fromdate:type:DataTypes.DATEONLY(),
-    todate:type:DataTypes.DATEONLY()
+    fromdate:{type:DataTypes.DATEONLY},
+    todate:{type:DataTypes.DATEONLY},
+    status:{type:DataTypes.INTEGER(1),defaultValue:1}
   }, {
     classMethods: {
       associate: function(models) {
+
+
+
       }
     }
   });
