9cd2ef592a3c1b1605c96be54d63ae019f419dae
ofituri <omar.fituri@gmail.com>
2015-12-13 18:15:54 +0200

    omar

---
+++ models/book.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Book = sequelize.define("Book", {
+    name: {type:DataTypes.STRING (60) , defaultValue:"Unknown"},
+    status:{type:DataTypes.INTEGER(1),defaultValue:1},
+   }, {
+    classMethods: {
+      associate: function(models) {
+
+      }
+    }
+  });
+
+  return Book;
+};
