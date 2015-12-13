3fbf0c9482c6f6d959f759f8c81af6acb0e9fa24
afituri <ahmed.elfituri@gmail.com>
2015-12-13 17:21:52 +0200

    first commit

---
+++ models/customer.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define("Customer", {
+    name: {type:DataTypes.STRING (60) , defaultValue:"Unknown name"},
+    status:{type:DataTypes.INTEGER(1),defaultValue:1},
+    email : DataTypes.STRING (150),
+    phone : DataTypes.STRING (11),
+    idNumber : DataTypes.STRING (12)
+  }, {
+    classMethods: {
+      associate: function(models) {
+
+      }
+    }
+  });
+
+  return Customer;
+};
