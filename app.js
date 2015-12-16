7dd47c579ab7ebf5498c1ec1ee9fed3b674c73fd
jelban <ejelban@gmail.com>
2015-12-16 18:39:01 +0200

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var author = require('./routes/author');
+var author = require('./routes/category');
 
 var app = express();
 
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/author', author);
-
+app.use('/category', category);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
