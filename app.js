ddc390a6c5e05a18ed43f1f55053de8e915cbd03
ofituri <omar.fituri@gmail.com>
2015-12-16 18:54:01 +0200

    add cutomer controller

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var author = require('./routes/author');
+var customer = require('./routes/customer');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/author', author);
+app.use('/customer', customer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/customer.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var customer = require('../controller/customer').customer;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  customer.getAll(function (result){
+    res.send(result);
+  });
+});
+
+/* GET home page. */
+router.get('/new', function(req, res, next) {
+  res.render("newCustomer");
+});
+
+
+router.get('/addCustomer', function  (req, res, next){
+  var obj1 = {
+    name : "mohamed",
+    email : "moh@yahoo.ly",
+    phone : "09119993831",
+    idNumber : "62230"
+  };
+  var obj2 = {
+    name : "Ali",
+    email : "Ali@gmail.ly",
+    phone : "09172873469",
+    idNumber : "634534"
+  };
+  customer.addCustomer(obj1 , obj2 , function(result){
+        res.send(result);
+  });
+});
+
+router.get('/editCustomer', function  (req, res, next){
+  var CustomerId = 3;
+  var obj = {
+    phone : "092999999",
+    };
+  customer.editCustomer(CustomerId, obj , function(result){
+    //console.log(result);
+    res.send(result);
+  });
+});
+
+router.get('/deleteCustomer', function  (req, res, next){
+  var CustomerId = 2;
+ 
+  customer.deleteCustomer(CustomerId , function(result){
+    res.send(result);
+  });
+});
+
+module.exports = router;
