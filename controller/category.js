94728c5c48e93162349932c107d5d3d9a51797e2
jelban <ejelban@gmail.com>
2015-12-16 19:03:39 +0200

    done2

---
+++ controller/category.js
@@ -0,0 +1,30 @@
+var models = require("../models");
+exports.category = {
+   /* add new category*/
+  addcategory : function(body,cb){
+    models.Category.create(body).then(function(result) {
+      cb(result);
+    });
+  },
+
+  editcategory : function (categoryId, obj , cb) {
+    models.Category.update(obj, {where : {id : categoryId}})
+      .then(function (result){
+        cb(result);
+      });
+  },
+  deletecategory : function (categoryId, cb) {
+    models.Category.destroy({where : {id : categoryId}})
+      .then(function (result){
+        cb(result);
+      });
+  },
+
+  getAll : function (cb) {
+    models.Category.findAndCountAll({where : {status :1}})
+      .then (function (result){
+        cb(result);
+      });
+  }
+
+};
